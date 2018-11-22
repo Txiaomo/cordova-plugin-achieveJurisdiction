e4b776450b9e4fe4ac41bb5bb692ec1058b99f41
xiaomo <tangjijun@1ziton.com>
2018-11-22 14:07:42 +0800

    删除

--- www/AchieveJurisdiction.js
+++
@@ -1,8 +0,0 @@
-var exec = require('cordova/exec');
-
-exports.storage = function(arg0, success, error) {
-    exec(success, error, "AchieveJurisdiction", "storage", [arg0]);
-};
-exports.exitApp = function(arg0, success, error) {
-    exec(success, error, "AchieveJurisdiction", "exitApp", [arg0]);
-};
